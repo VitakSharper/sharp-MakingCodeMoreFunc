ba0192ef4bce33e4068d6a06703fb0f40cd2aad9
VitakSharper <v.banard@gmail.com>
2020-07-04 22:39:08 +0200

    Finish implementing Immutable Object

---
+++ ImmutableObjects/MoneyAmount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ImmutableObjects
+{
+    // Impossible to reproduce aliasing bug on an immutable object;
+    internal sealed class MoneyAmount : IEquatable<MoneyAmount>
+    {
+        public decimal Amount { get; }
+        public string CurrencySymbol { get; }
+
+        public MoneyAmount(decimal amount, string currencySymbol)
+        {
+            Amount = amount;
+            CurrencySymbol = currencySymbol;
+        }
+
+        public MoneyAmount Scale(decimal factor) =>
+            new MoneyAmount(Amount * factor, CurrencySymbol);
+
+        // Avoid to overloads operator and let consumers rely on proper method calls;
+        public static MoneyAmount operator *(MoneyAmount amount, decimal factor) => amount.Scale(factor);
+
+        // obj can be a different object than MoneyAmount , let cast it to MoneyAmount;
+        public override bool Equals(object obj) =>
+            Equals(obj as MoneyAmount);
+
+        // Value-typed equality - Two objects are equal if their types are the same and their contained values are the same;
+        public bool Equals(MoneyAmount other) =>
+            other != null &&
+            Amount == other.Amount &&
+            CurrencySymbol == other.CurrencySymbol;
+
+        // Whenever you override Equals() method, you have to override GetHashCode() as well;
+        // ^ OR exclusive XOR
+        public override int GetHashCode() =>
+            Amount.GetHashCode() ^ CurrencySymbol.GetHashCode();
+
+        public static bool operator ==(MoneyAmount a, MoneyAmount b) =>
+            (ReferenceEquals(a, null) && ReferenceEquals(b, null)) ||
+            (!ReferenceEquals(a, null) && a.Equals(b));
+
+        public static bool operator !=(MoneyAmount a, MoneyAmount b) => !(a == b);
+
+        public override string ToString() => $"{Amount} {CurrencySymbol}";
+    }
+}
---
+++ ImmutableObjects/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace ImmutableObjects
+{
+    internal static class Program
+    {
+        private static bool IsHappyHour { get; set; }
+
+
+        // Reference types are susceptible to aliasing bugs;
+        // By changing contents of an existing object we are risking an aliasing bug to apear;
+        // Try to avoid instantiating objects directly;
+        // Modification to the constructor requires update in all consumers;
+        // Let objects of the class construct subsequent objects;
+        private static MoneyAmount Reserve(MoneyAmount cost)
+        {
+            decimal factor = 1;
+            var newCost = cost;
+
+            if (IsHappyHour)
+            {
+                // Create new object when content should change;
+                factor = .5M;
+            }
+
+            Console.WriteLine($"\nReserving an item that costs {cost}");
+            return cost.Scale(factor);
+        }
+
+        private static void Buy(MoneyAmount wallet, MoneyAmount cost)
+        {
+            var enoughMoney = wallet.Amount >= cost.Amount;
+
+            var finalCost = Reserve(cost);
+
+            var finalEnough = wallet.Amount >= finalCost.Amount;
+
+            if (enoughMoney && finalEnough)
+                Console.WriteLine($"You will pay {cost} with your {wallet}.");
+            else if (finalEnough)
+                Console.WriteLine($"This time, {wallet} will be enough to pay {finalCost}");
+            else
+                Console.WriteLine($"You cannot pay {cost} with your {wallet}.");
+        }
+
+        private static void Main()
+        {
+            //Buy(
+            //    new MoneyAmount(100, "USD"),
+            //    new MoneyAmount(70, "USD")
+            //);
+
+            //IsHappyHour = true;
+            //Buy(
+            //    new MoneyAmount(100, "USD"),
+            //    new MoneyAmount(120, "USD")
+            //);
+
+            var x = new MoneyAmount(5, "USD");
+            var y = new MoneyAmount(5, "USD");
+
+            if (x == y)
+                Console.WriteLine("Equal.");
+            if ((x * 2) == y)
+                Console.WriteLine("Equal after scaling.");
+
+            //var x = 2;
+            //var y = 2;
+
+            //var set = new HashSet<MoneyAmount>();
+
+            //set.Add(x);
+
+            //if (set.Contains(y))
+            //    Console.WriteLine("Cannot add repeated value.");
+            //else
+            //    set.Add(y);
+
+
+            //Console.WriteLine($"Count = {set.Count}");
+        }
+    }
+}
